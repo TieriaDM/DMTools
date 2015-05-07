190bf930dd41a9eafc45c0e519da72dc9de16cc9
TieriaDM <tieria0512@163.com>
2015-05-07 23:41:21 +0800

    Initial Commit

---
+++ DMTools/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DMTools
+//
+//  Created by Tieria on 15/5/7.
+//  Copyright (c) 2015年 Tieria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DMTools/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DMTools
+//
+//  Created by Tieria on 15/5/7.
+//  Copyright (c) 2015年 Tieria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
